a3479880ee6db882801e3994e241428646e65e4e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-13 10:18:33 -0700

    we created/populated our profile data

---
+++ src/services/profile-data.js
@@ -0,0 +1,14 @@
+const profile1 = {
+    id: 1,
+    name: 'Alex',
+    bio: `
+        My name is Alex. I am a giant kid in a grown-up body. I like colors!
+        I do not like clutter.
+    `,
+    image: 'Alex.jpg',
+    keywords: {
+        stickers: 'I like the colors.',
+        harryPotter: 'It is my favorite movie.',
+    }
+};
+export default profile1;
