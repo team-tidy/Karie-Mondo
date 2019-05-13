7fcf479c2e4cf595165600c4c93fcd98fe5865c4
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-13 15:56:41 -0700

    added test for false in matchMaker test file

--- test/matchMaker.test.js
+++ test/matchMaker.test.js
@@ -28,3 +28,28 @@ test('comparing profile and item keys, testing for true on match', (assert) => {
     // assert
     assert.equal(result, expected);
 });
+
+test('comparing profile and item keys, testing for false on mismatch', (assert) => {
+    // arrange
+    const item1 = {
+        keywords: {
+            fun: true,
+            colorful: true
+        }
+    };
+
+    const profile1 = {
+        keywords: {
+            stickers: 'I like the colors.',
+            kid: 'I am a giant kid.'
+        }
+    };
+    
+    const expected = false;
+
+    // act
+    const result = matchMaker(item1, profile1);
+
+    // assert
+    assert.equal(result, expected);
+});
