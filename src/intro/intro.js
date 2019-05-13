8141998bde6ee1ce59eaa00ff128ebba1676c883
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-13 11:52:55 -0700

    save user to local storage upon play button click

--- src/intro/intro.js
+++ src/intro/intro.js
@@ -1,5 +1,10 @@
+import makeUser from './make-user.js';
+import api from '../services/api.js';
+
 const playButton = document.getElementById('play-button');
 
 playButton.addEventListener('click', () => {
+    const user = makeUser();
+    api.saveUser(user);
     window.location = './game.html';
 });
