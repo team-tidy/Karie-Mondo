f48d6bbee680c67bd593e1303f293b118bbe4587
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-14 11:56:36 -0700

    updated score message and score to end page

--- src/result/results.js
+++ src/result/results.js
@@ -1,8 +1,12 @@
 import api from '../services/api.js';
 import loadUpdatedScore from '../load-updated-score.js';
+import resultsDictionary from '../services/results-dictionary.js';
+import finalScoreMessage from '../result/final-score-message.js';
 
 const avatarImage = document.getElementById('pic');
 const avatarName = document.getElementById('avatar-name');
+const finalMessageDom = document.getElementById('final-message');
+const finalScore = document.getElementById('final-score');
 
 const user = api.getUser();
 const profile = api.getProfile(user.id);
@@ -10,3 +14,9 @@ loadUpdatedScore();
 
 avatarImage.src = './assets/' + profile.image + '.jpg';
 avatarName.textContent = profile.name;
+
+const finalMessage = finalScoreMessage(user.score, resultsDictionary);
+finalMessageDom.textContent = finalMessage;
+
+const finalScoreString = 'You scored ' + user.score + ' points!';
+finalScore.textContent = finalScoreString;
---
+++ src/services/results-dictionary.js
@@ -0,0 +1,6 @@
+const resultsDictionary = { 
+    win: 'You Won!', 
+    okay: 'You did okay', 
+    lost: 'You lost' 
+};
+export default resultsDictionary;
