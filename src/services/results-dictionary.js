46dc624402278a231fd3af58c8e671c5cf0c19f6
aramirezdecruz3148 <leigh3148@gmail.com>
2019-05-15 10:42:56 -0700

    changed messages for outcomes

--- src/services/results-dictionary.js
+++ src/services/results-dictionary.js
@@ -1,6 +1,6 @@
 const resultsDictionary = { 
-    win: 'You Won!', 
-    okay: 'You did okay', 
-    lost: 'You lost' 
+    win: 'You did exceptional! You are great at picking up on clues!', 
+    okay: 'You did okay, you know a thing or two about your character.', 
+    lost: 'Hmm...maybe next time look for the clues between the lines. Try again?' 
 };
 export default resultsDictionary;
